610ae69e4e508afbdedc0cd9e5e342039cdc7002
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-04 19:21:54 -0300

    Crear config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuración de Chrome Magic Good
+API_TOKEN = "TU_TOKEN_DE_TELEGRAM"
+ADMIN_ID = "TU_ID_DE_USUARIO"
+
+# Parámetros de Hyper-Velocidad
+CLIP_DURATION = 60  # Segundos por clip
+TEMP_FOLDER = "temp_segments"
+MAX_THREADS = 4     # Para procesamiento paralelo si fuera necesario
