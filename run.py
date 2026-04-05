7f43d8e0643343066145784adb1d32d127a794a6
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-05 17:21:00 -0300

    Actualizar run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import server, main, threading, config
 from flask import request
 
-@server.app.route('/process', methods=['POST'])
-def handle_process():
-    link = request.form.get('link')
-    titulo = request.form.get('titulo')
-    
-    threading.Thread(target=main.motor_descarga_y_corte, args=(link, titulo)).start()
-    return "<h1>🚀 Descarga iniciada en segundo plano...</h1><script>setTimeout(()=>window.location='/',2000)</script>"
+@server.app.route('/run', methods=['POST'])
+def handle():
+    t = request.form.get('titulo')
+    l = request.form.get('link')
+    threading.Thread(target=main.motor_principal, args=(l, t)).start()
+    return "<h1>🚀 Descarga iniciada...</h1><script>setTimeout(()=>window.location='/',2000)</script>"
 
 if __name__ == "__main__":
+    print(f"🔥 Sistema Mally Pro en línea: http://localhost:{config.PORT}")
     server.app.run(host='0.0.0.0', port=config.PORT)
