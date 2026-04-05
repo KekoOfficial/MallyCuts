f9800911033510b1c17e4c972412e5180d04d2b4
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-05 17:14:05 -0300

    Actualizar run.py

--- run.py
+++ run.py
@@ -1,18 +1,13 @@
-import server, main, threading, os, config
+import server, main, threading, config
+from flask import request
 
-@server.app.route('/upload', methods=['POST'])
-def handle_upload():
-    video_file = server.request.files['file']
-    titulo = server.request.form.get('titulo')
+@server.app.route('/process', methods=['POST'])
+def handle_process():
+    link = request.form.get('link')
+    titulo = request.form.get('titulo')
     
-    save_path = "entrada_temp.mp4"
-    video_file.save(save_path)
-    
-    # Lanzamos el motor en un hilo independiente para no bloquear la web
-    threading.Thread(target=main.motor_procesamiento, args=(save_path, titulo)).start()
-    
-    return "<h1>🚀 Procesando en segundo plano...</h1><script>setTimeout(()=>window.location='/',2000)</script>"
+    threading.Thread(target=main.motor_descarga_y_corte, args=(link, titulo)).start()
+    return "<h1>🚀 Descarga iniciada en segundo plano...</h1><script>setTimeout(()=>window.location='/',2000)</script>"
 
 if __name__ == "__main__":
-    print(f"🔥 MALLY SERIES PRO activo en el puerto {config.PORT}")
-    server.app.run(host='0.0.0.0', port=config.PORT, debug=False)
+    server.app.run(host='0.0.0.0', port=config.PORT)
