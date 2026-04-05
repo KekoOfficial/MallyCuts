32c8dfcbd582b5d6043837a7a0b7c12c82535a43
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-05 17:20:12 -0300

    Actualizar bot_mally.py

--- bot_mally.py
+++ bot_mally.py
@@ -6,14 +6,9 @@ apihelper.READ_TIMEOUT = config.READ_TIMEOUT
 apihelper.CONNECT_TIMEOUT = config.CONNECT_TIMEOUT
 bot = telebot.TeleBot(config.API_TOKEN)
 
-def enviar_clip(video_path, thumb_path, caption):
-    with open(video_path, 'rb') as v:
-        with open(thumb_path, 'rb') as t:
-            return bot.send_video(
-                config.CHAT_ID, v, thumb=t, 
-                caption=caption, parse_mode="HTML", 
-                timeout=config.READ_TIMEOUT
-            )
+def enviar_clip(v_path, t_path, cap):
+    with open(v_path, 'rb') as v, open(t_path, 'rb') as t:
+        return bot.send_video(config.CHAT_ID, v, thumb=t, caption=cap, parse_mode="HTML")
 
-def enviar_texto(texto):
-    return bot.send_message(config.CHAT_ID, texto, parse_mode="HTML")
+def enviar_msg(txt):
+    return bot.send_message(config.CHAT_ID, txt, parse_mode="HTML")
