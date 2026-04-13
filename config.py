7f2c353774993f79ffae4cfe9f19b925e5e0aa38
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 12:25:09 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -2,10 +2,11 @@
 API_TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
 CHAT_ID = "-1003584710096"
 
-# Parámetros de Producción
+# Producción
 TEMP_FOLDER = "mally_studio_segments"
-CLIP_DURATION = 60  # Segundos
+CLIP_DURATION = 60  
 
-# Estética de Marca de Agua
+# Branding Central
 WATERMARK_TEXT = "t.me/MallySeries"
-WATERMARK_COLOR = "white@0.4"
+WATERMARK_COLOR = "white@0.5" # Más translúcido (30%) para no tapar la acción central
+WATERMARK_SIZE = 32
