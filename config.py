faceefad7798b32d52832ee676446f9aa8472fe6
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 07:27:36 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 API_TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
 CHAT_ID = "-1003584710096"
 
+# Directorios y Tiempos
 TEMP_FOLDER = "mally_studio_segments"
-# Corte de 3 minutos (180 segundos)
-CLIP_DURATION = 60
+CLIP_DURATION = 60 # 3 Minutos exactos
