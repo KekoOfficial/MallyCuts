250e3032b6c486cadce78aca772e188a7d850aaa
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 12:24:22 -0300

    Actualizar cortes.py

--- cortes.py
+++ cortes.py
@@ -2,13 +2,14 @@ import subprocess
 import config
 
 def extraer_segmento(path_original, n):
-    """Corta el segmento de 60 segundos exactos"""
     inicio = (n - 1) * config.CLIP_DURATION
     path_salida = f"{config.TEMP_FOLDER}/raw_{n}.mp4"
     
-    comando = [
+    # Usamos -accurate_seek para que el corte sea perfecto en el segundo indicado
+    cmd = [
         'ffmpeg', '-y', '-ss', str(inicio), '-t', str(config.CLIP_DURATION),
-        '-i', path_original, '-c', 'copy', '-avoid_negative_ts', '1', path_salida
+        '-i', path_original, '-c', 'copy', '-avoid_negative_ts', 'make_zero', 
+        path_salida
     ]
-    subprocess.run(comando, check=True, capture_output=True)
+    subprocess.run(cmd, check=True, capture_output=True)
     return path_salida
