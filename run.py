306d7c93e25bef8d6d2e309fe3f4bc5f72ce8f57
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 07:29:20 -0300

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-# Asegura que las carpetas existan antes de empezar
-os.makedirs("videos_galeria", exist_ok=True)
-os.makedirs("mally_studio_segments", exist_ok=True)
-
-print("--- INICIANDO SISTEMA OPERATIVO MP ---")
-subprocess.run(["python", "main.py"])
