97dda248c67ea98f12b4adc43bd78e14db4cb4e2
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 11:05:19 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 # --- MALLY SERIES CONFIG ---
 API_TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
 CHAT_ID = "-1003584710096"
-
-# Directorios y Tiempos
 TEMP_FOLDER = "mally_studio_segments"
-CLIP_DURATION = 60  # ACTUALIZADO A 60 SEGUNDOS
-
-# Configuración de Marca de Agua
+CLIP_DURATION = 60 
 WATERMARK_TEXT = "t.me/MallySeries"
-WATERMARK_COLOR = "white@0.4" 
+WATERMARK_COLOR = "white@0.4"
