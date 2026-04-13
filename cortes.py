6b194de8369cd740534d003376b6e5f1afb851b8
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-13 17:18:49 -0300

    Actualizar cortes.py

--- cortes.py
+++ cortes.py
@@ -2,10 +2,10 @@ import subprocess
 import config
 
 def extraer_segmento(path_original, n):
+    """Extrae 60s sin procesar para pasar al cerebro de edición"""
     inicio = (n - 1) * config.CLIP_DURATION
     path_salida = f"{config.TEMP_FOLDER}/raw_{n}.mp4"
     
-    # Usamos -accurate_seek para que el corte sea perfecto en el segundo indicado
     cmd = [
         'ffmpeg', '-y', '-ss', str(inicio), '-t', str(config.CLIP_DURATION),
         '-i', path_original, '-c', 'copy', '-avoid_negative_ts', 'make_zero', 
