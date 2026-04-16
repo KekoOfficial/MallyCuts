6103af64cd7ef61aa245ea626a5ce7e598d37fd5
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-16 17:54:33 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
-# config.py
 import os
 
+# --- MALLY SERIES CONFIG ---
 BOT_TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
 CHAT_ID = "-1003584710096"
 
+# Producción Express
 TEMP_FOLDER = "mally_studio_segments"
 CLIP_DURATION = 60  
+
+# Branding Unificado
 WATERMARK_TEXT = "t.me/MallySeries"
 WATERMARK_COLOR = "white@0.5"
 WATERMARK_SIZE = 32
+
+# Inteligencia de Red
 MAX_RETRIES = 3
-TIMEOUT_SEND = 300
+TIMEOUT_SEND = 300 
 
 if not os.path.exists(TEMP_FOLDER):
     os.makedirs(TEMP_FOLDER)
