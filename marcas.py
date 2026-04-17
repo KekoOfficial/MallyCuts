5bbbde2aa7d664b743ab165098585b1146d420e7
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-16 23:46:14 -0300

    Delete marcas.py

--- marcas.py
+++
@@ -1,9 +0,0 @@
-import config
-
-def obtener_filtro():
-    """Retorna la cadena de filtro para FFmpeg"""
-    return (
-        f"drawtext=text='{config.WATERMARK_TEXT}':x=10:y=H-th-10:"
-        f"fontcolor={config.WATERMARK_COLOR}:fontsize={config.WATERMARK_SIZE}:"
-        f"shadowcolor=black@0.6:shadowx=2:shadowy=2"
-    )
