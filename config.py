69f3684e73e347be02ed2531e0de8469042cdfa5
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-17 10:16:06 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
-# --- CONFIGURACIÓN MALLY CUTS ---
-TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
-CHAT_ID = "-1003584710096"
+import os
+
+# 🔐 Seguridad: Busca en el sistema, si no lo halla, usa tus IDs
+TOKEN = os.getenv("BOT_TOKEN", "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU")
+CHAT_ID = os.getenv("CHAT_ID", "-1003584710096")
+
+# 📁 Carpetas
 UPLOAD_FOLDER = "videos"
-PORTADA_FOLDER = "static" # Carpeta para guardar la portada seleccionada
+PORTADA_FOLDER = "static"
+FOTO_PORTADA = os.path.join(PORTADA_FOLDER, "temp_portada.jpg")
+
+# ⚡ Config extra (Potencia MallyCuts)
+MAX_WORKERS = 4          # Cortes en paralelo
+TIMEOUT_TELEGRAM = 300   # Tiempo límite de subida
+REINTENTOS = 3           # Reintentos si falla la red
