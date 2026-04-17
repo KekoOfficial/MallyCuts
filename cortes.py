edbe5346596b07738d83cb9a70fb45c9d283a432
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-17 00:34:06 -0300

    Actualizar cortes.py

--- cortes.py
+++ cortes.py
@@ -1,15 +1,16 @@
 import ffmpeg
-import os
+import config
 
-def procesar_segmento(video_in, foto_in, inicio, duracion, salida):
-    """Corta un segmento de 60s e inyecta la portada."""
+def procesar_segmento(v_in, inicio, duracion, salida):
+    """Corta 60s e inyecta la portada flash."""
     try:
-        (ffmpeg.input(video_in, ss=inicio, t=duracion)
-         .overlay(ffmpeg.input(foto_in), enable='between(t,0,0.05)')
+        (ffmpeg.input(v_in, ss=inicio, t=duracion)
+         .overlay(ffmpeg.input(config.FOTO_PORTADA), enable='between(t,0,0.05)')
          .output(salida, vcodec='libx264', preset='ultrafast', acodec='aac',
                  video_bitrate='3.8M', maxrate='4M', bufsize='6M',
                  pix_fmt='yuv420p', loglevel="error")
          .overwrite_output().run(quiet=True))
         return True
-    except:
+    except Exception as e:
+        print(f"Error en FFmpeg: {e}")
         return False
