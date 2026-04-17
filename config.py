b67e791cf54eecc2d25505075d5b66c39a765f9a
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-17 03:20:06 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+# --- CONFIGURACIÓN MALLY CUTS ---
 TOKEN = "8759783698:AAFUuC67X--qXoqD4D2YQ7RYlPlHoQmoYlU"
 CHAT_ID = "-1003584710096"
-FOTO_PORTADA = "foto.jpg"
 UPLOAD_FOLDER = "videos"
+PORTADA_FOLDER = "static" # Carpeta para guardar la portada seleccionada
