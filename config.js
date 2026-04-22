810d17117d189ad97a5b917075c6111939b94390
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-22 11:28:19 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
 // 🤖 BOT DE TELEGRAM
 module.exports = {
     TOKEN: "8459092113:AAFFJ0b7H5gFzYjgYGk_g_57cI709dhVRhI",
-    // 📢 Lista de canales a los que se enviará el contenido (agregá o quitá los que quieras)
-    CHAT_IDS: [
-        "-1003983527231",
-        "-1003706372741"
-    ],
+
+    // 📢 CONFIGURACIÓN DE CANALES
+    CANAL_MIO: {
+        ID: "-1003983527231",       // Este es tu canal, aparecerá en el mensaje
+        NOMBRE: "@MallyUmbrae"      // Poné el nombre o enlace que quieras que se muestre
+    },
+    CANAL_PRIVADO: {
+        ID: "-1003706372741"        // Este es el canal privado, AQUÍ SE GUARDARÁN LOS ARCHIVOS
+    },
 
     // ⚙️ AJUSTES GENERALES
     CLIP_DURATION: 60,        // Duración de cada parte en segundos
-    MAX_RETRIES: 3,           // Intentos de reenvío si falla
-    TIMEOUT_SEND: 300000,    // Tiempo máximo de espera al enviar (5 minutos)
+    MAX_RETRIES: 3,           // Cantidad de intentos si falla
+    TIMEOUT_SEND: 300000,    // Tiempo de espera máximo al enviar (5 minutos)
 
-    // 📂 RUTAS DE ARCHIVOS
+    // 📂 Rutas de carpetas
     TEMP_FOLDER: require('path').join(__dirname, 'videos', 'output')
 };
