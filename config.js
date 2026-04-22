64a6f2c83284c7b480b1ff5297559cf2eafe2fd2
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-22 11:22:37 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 // 🤖 BOT DE TELEGRAM
 module.exports = {
     TOKEN: "8459092113:AAFFJ0b7H5gFzYjgYGk_g_57cI709dhVRhI",
-    CHAT_ID: "-1003983527231",
-    CHAT_ID: "-1003706372741",
-    
+    // 📢 Lista de canales a los que se enviará el contenido (agregá o quitá los que quieras)
+    CHAT_IDS: [
+        "-1003983527231",
+        "-1003706372741"
+    ],
 
     // ⚙️ AJUSTES GENERALES
-    CLIP_DURATION: 60,       // Duración de cada parte en segundos
-    MAX_RETRIES: 3,          // Intentos de reenvío si falla
+    CLIP_DURATION: 60,        // Duración de cada parte en segundos
+    MAX_RETRIES: 3,           // Intentos de reenvío si falla
     TIMEOUT_SEND: 300000,    // Tiempo máximo de espera al enviar (5 minutos)
 
     // 📂 RUTAS DE ARCHIVOS
